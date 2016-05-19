8c6354b27b6c4d0a02f980eb7b6e2989b89372f3
Lee <leensutton@outlook.com>
2016-05-18 22:18:55 -0700

    Rebuilding upDown app.

--- UpDown/src/main/java/Application.java
+++ UpDown/src/main/java/Application.java
@@ -3,23 +3,62 @@
  */
 public class Application {
     public static void main(String[] args) {
-        // invoke runner
+        runner();
     }
 
-    // iterations
-    // upDirection
-    // downDirection
-    // currentDirection
+    private static Integer MAX_ITERATIONS = 1000;
+    private static String  UP_DIRECTION = "up";
+    private static String  DOWN_DIRECTION = "down";
+    private static String  currentDirection = "";
+    private static Integer iteration = 0;
+    private static String currentString = "";
 
     // runner
+    private static void runner(){
+        try{
+            checkIteration(iteration);
 
-    // check direction
+            checkDirection(currentString);
 
-    // check iterations
+            currentString = alterString(currentString);
 
-    // perform up/down and print
+            System.out.println(currentString);
 
-    // increment iteration
+            iteration++;
 
-    // call runner
+            Thread.sleep(50);
+
+            runner();
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private static void checkIteration(Integer iteration){
+        if(iteration >= MAX_ITERATIONS){
+            System.exit(1);
+        }
+    }
+
+    private static void checkDirection(String currentString) {
+        if(currentString.length() <= 0){
+            currentDirection = UP_DIRECTION;
+        }
+        if(currentString.length() >= 10){
+            currentDirection = DOWN_DIRECTION;
+        }
+    }
+
+    private static String alterString(String currentString){
+        String updatedString = currentString;
+        if(currentDirection.equals(UP_DIRECTION)){
+            updatedString += "-";
+        }
+        else {
+            updatedString = updatedString.substring(0, updatedString.length() - 1);
+        }
+        return updatedString;
+    }
 }
