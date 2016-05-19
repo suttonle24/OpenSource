bb22c003a8c1dc513fd034b694be348dfe770269
Lee <leensutton@outlook.com>
2016-05-18 17:21:05 -0700

    Adding fresh UpDown project

---
+++ UpDown/src/main/java/Application.java
@@ -0,0 +1,25 @@
+/**
+ * Created by lee on 5/18/16.
+ */
+public class Application {
+    public static void main(String[] args) {
+        // invoke runner
+    }
+
+    // iterations
+    // upDirection
+    // downDirection
+    // currentDirection
+
+    // runner
+
+    // check direction
+
+    // check iterations
+
+    // perform up/down and print
+
+    // increment iteration
+
+    // call runner
+}
